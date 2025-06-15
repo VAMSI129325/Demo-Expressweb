9b38bbec77a4bd23b4f8940789015f0b51fcaecc
BEERE VAMSI <beerevamsi0@gmail.com>
2025-06-15 19:52:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+//Static Middleware 
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname+'/home.html')
+})
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname+'/about.html')
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname+'/contact.html')
+})
+app.get('/connectus', (req, res) => {
+    res.sendFile(__dirname+'/contact.html')
+})
+app.get('/productapi', (req, res) => {
+    var mydata=(
+      [{"title":"hello"}]
+    )
+  })
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
